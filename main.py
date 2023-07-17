19c8e6c1122a4e2ca59c492354ad233808cccba1
jung in Kim <62374317+jungin-kim@users.noreply.github.com>
2023-07-17 13:22:42 +0900

    Basic Setting
    씬세팅 및 파이썬 버전 정리

---
+++ main.py
@@ -0,0 +1,13 @@
+'Python 3.7'
+from vpython import *
+from tkinter import *
+
+root = Tk()
+
+monitor_height = root.winfo_screenheight()
+monitor_width = root.winfo_screenwidth()
+
+scene = canvas(width = monitor_width-15, height = monitor_height-15, resizable = False, background = color.black)
+box()
+earth = sphere(pos=vec(0, 0, 0), radius=10, texture=textures.earth)
+orbit = ring(pos=vector(0, 0, 0), axis=vector(0, 1, 0), radius=12, color=color.red, thickness=0.1)
