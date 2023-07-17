6fd94c85a031072edea7d03dcebb561149399f6a
jung in Kim <62374317+jungin-kim@users.noreply.github.com>
2023-07-17 13:28:03 +0900

    Basic Settiing modify
    resizable 세팅 변경

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ root = Tk()
 monitor_height = root.winfo_screenheight()
 monitor_width = root.winfo_screenwidth()
 
-scene = canvas(width = monitor_width-15, height = monitor_height-15, resizable = False, background = color.black)
+scene = canvas(width = monitor_width-15, height = monitor_height-15)
+scene.resizable = False
 box()
 earth = sphere(pos=vec(0, 0, 0), radius=10, texture=textures.earth)
 orbit = ring(pos=vector(0, 0, 0), axis=vector(0, 1, 0), radius=12, color=color.red, thickness=0.1)
