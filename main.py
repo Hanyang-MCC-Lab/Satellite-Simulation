c0c05454837b88aa3cb02e0f4d530df039100cda
jung in Kim <62374317+jungin-kim@users.noreply.github.com>
2023-07-18 15:04:48 +0900

    First satelite orbit settlement
    첫번째 위성궤도 안착 성공

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ CONST_SAT_RADIUS = CONST_EARTH_RADIUS + 550 #지구반경 + 550KM
 CONST_ORBIT_NUM = 72 #궤도개수
 CONST_SAT_NUM = 22 #위성개수
 CONST_ORBIT_ROT = 0.087 #궤도회전각도 라디안 5도
-CONST_SAT_ROT = 0.28 #위성회전각도 라디안 16.3도
+CONST_SAT_ROT = 0.285 #위성회전각도 라디안 16.3도
 
 #궤도 및 위성 리스트 생성
 orbit = []
@@ -32,13 +32,14 @@ scene.resizable = False
 
 # xy평면과 x, y, z축
 mybox = box(pos=vec(0, 0, 0), length=30000, height=1, width=30000, opacity=0.5)
-lineX = arrow(pos=vector(-15000, 0, 0), axis=vector(1, 0, 0), shaftwidth=50, headwidth=300, headlength=300, length=30000, color=color.magenta)
-lineY = arrow(pos=vector(0, 0, -15000), axis=vector(0, 0, 1), shaftwidth=50, headwidth=300, headlength=300, length=30000, color=color.blue)
-lineZ = arrow(pos=vector(0, -10000, 0), axis=vector(0, 1, 0), shaftwidth=50, headwidth=300, headlength=300, length=20000, color=color.green)
+lineX = arrow(pos=vec(-15000, 0, 0), axis=vec(1, 0, 0), shaftwidth=50, headwidth=300, headlength=300, length=30000, color=color.magenta)
+lineY = arrow(pos=vec(0, 0, -15000), axis=vec(0, 0, 1), shaftwidth=50, headwidth=300, headlength=300, length=30000, color=color.blue)
+lineZ = arrow(pos=vec(0, -10000, 0), axis=vec(0, 1, 0), shaftwidth=50, headwidth=300, headlength=300, length=20000, color=color.green)
+t1 = text(pos=vec(-15000, 500, 0), text = "Vernal equinox", billboard = True, color=color.cyan, emissive = True)
 
 earth = sphere(pos=vec(1, 0, 0), radius=CONST_EARTH_RADIUS, texture=textures.earth) #지구생성
 
 for i in range(CONST_ORBIT_NUM) :
     orbit.append(ring(pos=vec(0, 0, 0), axis=vec(math.cos(CONST_ORBIT_ROT * i) * 0 - math.sin(CONST_ORBIT_ROT * i) * math.sin(inclination), math.cos(inclination), math.sin(CONST_ORBIT_ROT * i) * 0 + math.cos(CONST_ORBIT_ROT * i) * math.sin(inclination)), radius=CONST_SAT_RADIUS, color=color.red, thickness=15))
     for j in range(CONST_SAT_NUM): #위성생성
-        sat.append(sphere(pos=vec(CONST_SAT_RADIUS, 0, 0), axis=vec(0, math.cos(inclination / 180), math.sin(inclination / 180)), radius=60, color=color.white))
+        sat.append(sphere(pos=vec(CONST_SAT_RADIUS * math.cos(CONST_SAT_ROT * j), 0 - math.sin(inclination) * (CONST_SAT_RADIUS * math.sin(CONST_SAT_ROT * j)), math.cos(inclination) * (CONST_SAT_RADIUS * math.sin(CONST_SAT_ROT * j))), axis=vec(1, 0, 0), radius=60, color=color.white))
