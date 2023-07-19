8c69364be1ee24f734ff2fc4be5d2575d8734be2
KSG072 <tjrnjs123@gmail.com>
2023-07-19 15:14:43 +0900

    [FEAT] satellite.py 위성클래스 구현

--- satellite.py
+++ satellite.py
@@ -1,9 +1,13 @@
 import math
+from vpython import *
 from operator import mul
+
+
 class Satellite:
     # 위성 객체의 attribute
-
-    id = "undefined"
+    sat_attr = sphere()
+    orbit = None
+    id = "SAT-"
     # 위도, 경도, 고도(지구 반지름 + LEO 평균 고도)
     longitude = 0
     latitude = 0
@@ -11,23 +15,35 @@ class Satellite:
     # ECEF 좌표계상의 x, y, z좌표
     x, y, z = 0, 0, 0
 
-    def __init__(self, index, inclination, omega):
-        self.id = index
-        # 위도, 경도 구하는 공식
-        self.latitude = math.asin(mul(math.sin(inclination), math.sin(omega)))
-        self.longitude = math.atan2()
-
+    def __init__(self, orbit, sat_index, inclination, omega):
+        self.id = self.id + sat_index
+        self.orbit = orbit
+        # 위도, 경도
+        self.latitude = math.asin(math.sin(inclination) * math.sin(omega))
+        self.longitude = math.atan2(
+            math.cos(inclination) * math.sin(omega) * math.cos(omega) + 360) % 360 + orbit.ascend_lon
+        # ECEF 좌표
+        self.x = math.cos(self.latitude) * math.cos(self.longitude) * self.altitude
+        self.y = math.cos(self.latitude) * math.sin(self.longitude) * self.altitude
+        self.z = math.sin(self.latitude) * self.altitude
+        # 구체 attribute 설정
+        self.sat_attr.pos = vec(self.y, self.z, self.x)
+        self.sat_attr.axis = vec(1, 0, 0)  # 바라보는 방향이라는데 정확한 의미는 모름
+        self.sat_attr.radius = 60
+        self.sat_attr.color = color.white
 
     def get_geo_info(self):
         info = {
-            "lon" : self.longitude,
-            "lat" : self.latitude,
-            "alt" : self.altitude
+            "lon": self.longitude,
+            "lat": self.latitude,
+            "alt": self.altitude
         }
         return info
-    def get_ECEF_info(self):
+
+    def get_ecef_info(self):
         info = {
-            "x" : self.x,
-            "y" : self.y,
-            "z" : self.z,
+            "x": self.x,
+            "y": self.y,
+            "z": self.z,
         }
+        return info
